e88eeb4a8b600d15535a3e52ff0e74a564ec14b1
Hannibal96 <33417361+Hannibal96@users.noreply.github.com>
2021-06-24 17:24:29 +0300

    divided code

---
+++ Generator.py
@@ -0,0 +1,78 @@
+from DataLoader import *
+
+
+# generate caption
+def get_caps_from(features_tensors, model):
+    # generate the caption
+    model.eval()
+    with torch.no_grad():
+        features = model.encoder(features_tensors.to(device))
+        caps, alphas = model.decoder.generate_caption(features, vocab=dataset.vocab)
+        caption = ' '.join(caps)
+        show_image(features_tensors[0], title=caption)
+
+    return caps, alphas
+
+
+# Show attention
+def plot_attention(img, result, attention_plot):
+    # untransform
+    img[0] = img[0] * 0.229
+    img[1] = img[1] * 0.224
+    img[2] = img[2] * 0.225
+    img[0] += 0.485
+    img[1] += 0.456
+    img[2] += 0.406
+
+    img = img.numpy().transpose((1, 2, 0))
+    temp_image = img
+
+    fig = plt.figure(figsize=(15, 15))
+
+    len_result = len(result)
+    for ll in range(len_result):
+        temp_att = attention_plot[ll].reshape(7, 7)
+
+        ax = fig.add_subplot(len_result // 2, len_result // 2, ll + 1)
+        ax.set_title(result[ll])
+        img = ax.imshow(temp_image)
+        ax.imshow(temp_att, cmap='gray', alpha=0.7, extent=img.get_extent())
+
+    plt.tight_layout()
+    plt.show()
+
+
+data_location = '.'
+
+transforms = T.Compose([T.Resize(226), T.RandomCrop(224), T.ToTensor(), T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+
+
+dataset = FlickrDataset(root_dir=data_location+"/Images", captions_file=data_location+"/captions.txt",
+                        transform=transforms)
+print("Finished building the Dataset.")
+
+
+pad_idx = dataset.vocab.stoi["<PAD>"]
+
+data_loader = DataLoader(
+    dataset=dataset,
+    batch_size=1,
+    num_workers=0,
+    shuffle=True,
+    collate_fn=CapsCollate(pad_idx=pad_idx, batch_first=True)
+)
+
+# show any 1
+dataiter = iter(data_loader)
+images, _ = next(dataiter)
+
+model_name = './caption_model_E_1.torch'
+model = torch.load(model_name)
+
+img = images[0].detach().clone()
+img1 = images[0].detach().clone()
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+caps, alphas = get_caps_from(img.unsqueeze(0), model=model)
+
+plot_attention(img1, caps, alphas)
---
+++ Train.py
@@ -0,0 +1,127 @@
+from DataLoader import *
+
+
+def save_model(model, num_epochs):
+    model_state = {
+        'num_epochs': num_epochs,
+        'embed_size': embed_size,
+        'vocab_size': len(dataset.vocab),
+        'attention_dim': attention_dim,
+        'encoder_dim': encoder_dim,
+        'decoder_dim': decoder_dim,
+        'state_dict': model.state_dict()
+    }
+    path = "caption_model_E_"+str(num_epochs)+".torch"
+    torch.save(model, path)
+
+
+data_location = "."
+BATCH_SIZE = 32
+NUM_WORKER = 4
+
+# defining the transform to be applied
+transforms = T.Compose([
+    T.Resize(226),
+    T.RandomCrop(224),
+    T.ToTensor(),
+    T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+])
+
+
+dataset = FlickrDataset(root_dir=data_location+"/Images", captions_file=data_location+"/captions.txt",
+                        transform=transforms)
+print("Finished building the Dataset.")
+
+# Hyperparams
+embed_size = 300
+vocab_size = len(dataset.vocab)
+attention_dim = 256
+encoder_dim = 2048
+decoder_dim = 512
+learning_rate = 3e-4
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+#init model
+model = EncoderDecoder(
+    embed_size=300,
+    vocab_size=len(dataset.vocab),
+    attention_dim=256,
+    encoder_dim=2048,
+    decoder_dim=512
+).to(device)
+
+criterion = nn.CrossEntropyLoss(ignore_index=dataset.vocab.stoi["<PAD>"])
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+num_epochs = 1
+print_every = 100
+
+pad_idx = dataset.vocab.stoi["<PAD>"]
+
+data_loader = DataLoader(
+    dataset=dataset,
+    batch_size=BATCH_SIZE,
+    num_workers=0,
+    shuffle=True,
+    collate_fn=CapsCollate(pad_idx=pad_idx, batch_first=True)
+)
+
+loss_list = []
+perplexity_list = []
+total_loss = 0
+for epoch in range(1, num_epochs + 1):
+    for idx, (image, captions) in enumerate(iter(data_loader)):
+        image, captions = image.to(device), captions.to(device)
+
+        # Zero the gradients.
+        optimizer.zero_grad()
+
+        # Feed forward
+        outputs, attentions = model(image, captions)
+
+        # Calculate the batch loss.
+        targets = captions[:, 1:]
+        loss = criterion(outputs.view(-1, vocab_size), targets.reshape(-1))
+
+        # Backward pass.
+        loss.backward()
+
+        # Update the parameters in the optimizer.
+        optimizer.step()
+
+        total_loss += loss.item()
+
+        loss_list.append(loss.item())
+        if (idx + 1) % print_every == 0:
+            perplexity = total_loss / print_every
+            perplexity = np.exp(perplexity)
+            perplexity_list.append(perplexity)
+            # generate the caption
+            model.eval()
+            with torch.no_grad():
+                dataiter = iter(data_loader)
+                img, true_caption = next(dataiter)
+                true_caption = true_caption[0:1]
+                features = model.encoder(img[0:1].to(device))
+                caps, alphas = model.decoder.generate_caption(features, vocab=dataset.vocab)
+                caption = ' '.join(caps)
+                show_image(img[0], title=caption)
+            print("Epoch: {} loss: {:.5f}, perplexity: {:.5f}".format(epoch, loss.item(), perplexity))
+            total_loss = 0
+            model.train()
+
+    # save the latest model
+    save_model(model, epoch)
+    pickle.dump(loss_list, open("loss_list.p", "wb"))
+    pickle.dump(perplexity_list, open("perplexity_list.p", "wb"))
+
+plt.plot(loss_list)
+plt.title('Loss')
+plt.show()
+
+plt.plot(perplexity_list)
+plt.title('Perplexity')
+plt.show()
+
